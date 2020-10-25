37d1934cf41fb5c52099174731f2675898809f49
Adam Denes <adam.denes@icloud.com>
2020-10-25 15:34:45 +0100

    implementing basic record addition with type validation

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from csv_handler import Csv
 
 from collections import OrderedDict
+import datetime
 import sys
 import re
 import os
@@ -82,7 +83,54 @@ def menu():
 
 def add_record():
     """Add a record to the database"""
-    pass
+    first_row = []
+    answers = []
+
+    for header in list(Product.select().dicts()):
+        for key in header.keys():
+            if key not in first_row:
+                first_row.append(key)
+
+    for answer in first_row[1:4]:
+        while True:
+            try:
+                input_data = input(f'Enter {answer}: ')
+
+                if answer == 'product_name':
+                    if len(input_data) == 0:
+                        raise ValueError('Empty field is not allowed')
+                    elif re.match(r'\d+', input_data, re.I):
+                        raise ValueError(
+                            f'Numbers are not allowed for {answer}!')
+                    elif re.match(r'[^\w_]', input_data, re.I):
+                        raise ValueError(f'Only characters are accepted for {answer}!')
+                    else:
+                        answers.append(input_data)
+                        break
+                if answer == 'product_quantity':
+                    if re.match(r'[a-z|\W_]+', input_data, re.I):
+                        raise ValueError(
+                            f'Only numbers are accepted for {answer}!')
+                    else:
+                        answers.append(int(input_data))
+                        break
+                if answer == 'product_price':
+                    if re.match(r'[a-z|\W_]+', input_data, re.I):
+                        raise ValueError(
+                            f'Only numbers are accepted for {answer}!')
+                    else:
+                        input_data = re.sub(r'\W+', '', input_data)
+                        answers.append(int(input_data))
+                        break
+            except ValueError as ve:
+                print(ve)
+                continue
+
+    answers.append(datetime.datetime.now())
+    new_data = dict(zip(first_row[1:], answers))
+
+    with db.atomic():
+        Product.create(**new_data)
 
 
 def view_record():
@@ -123,8 +171,9 @@ def view_record():
 
 
 def backup_data():
-    """Backup database"""
-    pass
+    """Backup database to a csv file"""
+    query = Product.select().dicts()
+    Csv('backup.csv').write_csv(query)
 
 
 if __name__ == '__main__':

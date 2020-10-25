d942c3eb4a423dd453c7a655243d48436c62783a
Adam Denes <adam.denes@icloud.com>
2020-10-25 08:33:15 +0100

    implementing csv data import function

--- app.py
+++ app.py
@@ -11,27 +11,29 @@ db = SqliteDatabase('inventory.db')
 class Product(Model):
     product_id = IntegerField(primary_key=True)
     product_name = CharField(max_length=255)
-    product_quantity = IntegerField()
-    product_price = IntegerField()
+    product_quantity = IntegerField(default=0)
+    product_price = IntegerField(default=0)
     date_updated = DateTimeField()
 
     class Meta:
         database = db
 
+
 def import_csv():
     csv_obj = Csv('inventory.csv')
     cleaned_csv = csv_obj.clean_csv()
-    
-    for row in cleaned_csv:
-        print(row)
-        Product.create(product_name=row['product_name'])
-        Product.create(product_quantity=row['product_quantity'])
-        Product.create(product_price=row['product_price'])
-        Product.create(date_updated=row['date_updated'])
+
+    # wrapping bulk insert transaction with atomic()
+    # to speed up process
+    with db.atomic():
+        for data in cleaned_csv:
+            Product.insert_many(data).execute()
+
 
 def initialize():
     db.connect()
     db.create_tables([Product], safe=True)
+
 
 if __name__ == '__main__':
     initialize()
